e9761ee3d44aaeaee4f6cfef2305f1fd46db1b86
CJ Young <chrjyoung@gmail.com>
2026-06-11 12:54:12 -0400

    Add Symfony HTTP error handling and implement default custom error

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,10 +3,12 @@
 namespace App\EventListener;
 
 use App\Exception\ApiException;
+use App\Exception\InternalServerException;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 #[AsEventListener(event: KernelEvents::EXCEPTION, method: 'onKernelException', priority: 10)]
@@ -21,17 +23,17 @@ class ExceptionListener
         $exception = $event->getThrowable();
 
         if ($exception instanceof ApiException) {
-            $errorDetails = $exception->getDetails();
-            $errorCode = $exception->getErrorCode();
-            $httpStatus = $exception->getCode();
-
-            $response = [
-                'errorCode' => $errorCode,
-                'details' => $errorDetails,
-            ];
-
-            $event->setResponse(new JsonResponse($response, $httpStatus));
+            $event->setResponse($this->getCustomExceptionJsonResponse($exception));
             return;
+        }
+        
+        // Handle Symfony HTTP exceptions
+        // In the future, `errorCode` could be more semantic based on specific HTTP error
+        if ($exception instanceof HttpExceptionInterface) {
+            $event->setResponse(new JsonResponse([
+                'errorCode' => 'HTTP_ERROR',
+                'details' => [],
+            ], $exception->getStatusCode()));
         }
 
         // Preserve default Symfony debug responses when error is unexpected
@@ -39,11 +41,21 @@ class ExceptionListener
             return;
         }
 
-        $response = [
-            'errorCode' => 'INTERNAL_SERVER_ERROR',
-        ];
+        $event->setResponse(
+            $this->getCustomExceptionJsonResponse(new InternalServerException($exception))
+        );
+    }
+
+    private function getCustomExceptionJsonResponse(ApiException $exception) {
+        $errorDetails = $exception->getDetails();
+        $errorCode = $exception->getErrorCode();
         $httpStatus = $exception->getCode();
 
-        $event->setResponse(new JsonResponse($response, $httpStatus));
+        $response = [
+            'errorCode' => $errorCode,
+            'details' => $errorDetails,
+        ];
+
+        return new JsonResponse($response, $httpStatus);
     }
 }
